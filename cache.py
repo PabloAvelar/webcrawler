5f68d29d4b9d624b04260f453d073d8f55e2c50e
PabloAvelar <pabloarmenta84@gmail.com>
2024-05-12 19:52:37 -0600

    recuperando información con palabras clave con SQL en la bd

--- cache.py
+++ cache.py
@@ -3,24 +3,35 @@ from connection import connection
 cur = connection()
 
 
-def cache(*args):
-    query = "SELECT * FROM search"
+def cache(keywords: list) -> tuple:
+    """s
+    Función para buscar información con palabras clave en la base de datos como caché.
+
+    Recibe: list -> Lista de palabras clave para buscar
+    Devuelve: tuple -> Tupla con las coincidencias de la búsqueda con palabras clave en la base de datos
+
+    """
+
+    base_query = """
+    SELECT *
+    FROM `search` s
+    WHERE 
+    """
+
+    like_kw = ""
+    for i in range(len(keywords)):
+        like_kw += f'description LIKE "%{keywords[i]}%"'
+        if i < len(keywords) - 1:
+            like_kw += " or "
+
+    query = base_query + like_kw + ";"
+
     cur.execute(query)
 
     return cur.fetchall()
 
 
-data = cache()
-keywords = ["méxico"]
-results = []
-
-for row in data:
-    for kw in keywords:
-        if kw in row[1].lower():
-            results.append(row)
-
-
-print("Resultados:")
-for result in results:
-    print("\t" + result[1])
-    print(result[2])
+keywords = ["justicia", "ley"]
+print("resultados")
+for x in cache(keywords):
+    print(x)
